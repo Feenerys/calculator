a7eb61463e734ec4bd4208c6e0cb5444c9798898
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-02-04 14:15:46 +1100

    feat: add logic for calculating

--- script.js
+++ script.js
@@ -16,14 +16,67 @@ const divide = function(a,b) {
   };   
 
 const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".operators");
+const equalButton = document.querySelector("#equals");
+
 const input = document.querySelector("input");
-let current = 0;
+let selectedValue = "";
+let firstValue = "";
+let secondValue = "";
+let operation = "";
 
 numberButtons.forEach((button) => button.addEventListener("click", () => {
-  const number = button.id
-  input.value = number
-  current = number
+  const number = button.id;
+  input.value = number;
+  selectedValue = number;
 }));
 
+operatorButtons.forEach((button) => button.addEventListener("click", () => {
+  if (firstValue === "") {
+    firstValue = selectedValue;
+    selectedValue = "";
+  };
 
+  if (operation === "") {
+    selectedValue = "";
+    operation = button.id;
+  } else if (firstValue !== "" && selectedValue !== "") {
+    calculate();
+    operation = button.id;
+  } else {
+    operation = button.id
+  }
+}));
+
+equalButton.addEventListener("click", calculate)
+
+
+function calculate() {
+  secondValue = selectedValue;
+  
+  if (operation !== "") {
+    let result = 0;
+    
+    switch (operation) {
+      case "add":
+        result = add(firstValue,secondValue);
+        break;
+      case "subtract":
+        result = subtract(firstValue,secondValue);
+        break;
+      case "multiply":
+        result = multiply(firstValue,secondValue);
+        break;
+      case "divide":
+        result = divide(firstValue,secondValue);
+        break;
+    }
+    [input.value, firstValue] = [result, result];
+    secondValue = "";
+    operation = "";
+    selectedValue = "";
+  }
+}
+
+// TODO: clear button, pressing digits adds to the string,
 
