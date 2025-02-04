389ef57dba6e2253d803202b6171cbdce54f7ae8
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-02-04 14:14:25 +1100

    fix: allow strings to be operated on

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const add = function(a,b) {
-	return a + b
+	return +a + +b
 };
 
 const subtract = function(a,b) {
-	return a - b
+	return +a - +b
 };
 
 const multiply = function(a,b) {
-    return a * b
+    return +a * +b
   };
 
 const divide = function(a,b) {
     if (b === 0) {return "no"};
-    return a/b
+    return +a/+b
   };   
 
 const numberButtons = document.querySelectorAll(".numbers");
